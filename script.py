71364212e1c7cecbc2c46b9cd6f61c4913e6d6d4
Fangping Lan <34638594+lanfangping@users.noreply.github.com>
2025-04-29 21:33:33 -0400

    foo

--- script.py
+++ script.py
@@ -1,33 +1,15 @@
 import xarray as xr
+import pandas as pd
 
-# Load the dataset
-ds = xr.open_dataset("tas_day_CESM2_piSST_r1i1p1f1_gn_00110101-00201231.nc")
+def example(file_name, variable):
+    ds = xr.open_dataset(f"{file_name}.nc")
+    tas_data = ds[variable]
 
-# Overview of the dataset
-print(ds)
+    # Convert directly to pandas DataFrame
+    df = tas_data.to_dataframe().reset_index()
+    
+    df.to_json(f"{file_name}.json", orient='records')
+    
 
-# List variables (e.g., 'tas', 'pr', 'psl', etc.)
-print(ds.data_vars)
-
-# Access a variable
-tas = ds['tas']  # for example, surface air temperature
-
-# See dimensions
-print(tas.dims)
-
-# # Look at data values
-# print(tas.values)
-
-
-# from netCDF4 import Dataset
-
-# # Open file
-# ds = Dataset('tas_day_CESM2_piSST_r1i1p1f1_gn_00110101-00201231.nc', 'r')
-# print(ds)
-
-# # List variables
-# print(ds.variables.keys())
-
-# # Access variable data
-# temperature = ds.variables['lat_bnds'][:]
-# print(temperature.shape)
+if __name__ == '__main__':
+    example(file_name="tas_day_CESM2_piSST_r1i1p1f1_gn_00110101-00201231", variable='tas')
